d58fc722e6f2cff73b37fd0d04a8ce451ea58805
jacob43293 <jacob.43293@gmail.com>
2025-10-16 20:55:47 -0400

    Updated PotionRecipe and TimerUI WIP

--- Assets/Scripts/PotionRecipe.cs
+++ Assets/Scripts/PotionRecipe.cs
@@ -3,8 +3,8 @@ using UnityEngine;
 [CreateAssetMenu(fileName = "PotionRecipe", menuName = "Scriptable Objects/PotionRecipe")] 
 public class PotionScriptableObject : ScriptableObject {
     public string potionName;
-    public string firstIngredient;
-    public string secondIngredient;
-    public string thirdIngredient;
+    public IngredientSO firstIngredient;
+    public IngredientSO secondIngredient;
+    public IngredientSO thirdIngredient;
 }
 
